fd31c307746770d7735e4c5064ab117792b347e9
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-10-02 12:11:00 -0400

    not sure if this works

---
+++ Parser.java
@@ -0,0 +1,30 @@
+package mud;
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+	
+	public static String parse(Character playerOne, String input) {
+		List<String> splitStrings = Arrays.asList(input.split(" "));
+		
+		if (splitStrings.get(0).equals("exit")) {
+			System.exit(0);
+			String r = "Exiting the game...";
+		}
+		else if (splitStrings.get(0).equals("move")) {
+			String r = playerOne.move(splitStrings.get(1));
+		}
+		
+		else if (splitStrings.get(0).equals("get")) {
+			String r = playerOne.addItem(splitStrings.get(1));
+		}
+		else if (splitStrings.get(0).equals("drop")) {
+			String r = playerOne.removeItem(splitStrings.get(1));
+		}
+		else {
+			String r = "Oops! I don't know what you're trying to say!";
+		}
+		return r;
+	}
+
+}
