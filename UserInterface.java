4bead5757d36bd0d492ba15de85653db04618c02
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-10-03 12:29:43 -0400

    deleting commented out code

--- UserInterface.java
+++ UserInterface.java
@@ -10,38 +10,28 @@ public class UserInterface {
 		
 		Game.start();
 
-		//**
- 		String start = "Hello! Your name is Bob and you are in the Nice Room.";
- 		System.out.println(start);
- 		input = new Scanner(System.in);
- 		String direction = input.next();
-		direction = direction.toString();
-		String r = Game.person.move(direction);
-		System.out.println(r);
-		**/
-		
 		input = new Scanner(System.in);
 		System.out.println("What is your name?");
-		String name = input.next();
+		String name = input.nextLine();
 
 		Character playerOne = new Character(name, "You have no description yet.", Game.room1, null);
 		
-		System.out.println(Game.room1.getName());
-		
 		System.out.println("Hello " + name + "! You are in " + Game.room1.name);
+		
 		boolean end = false;
 		while (!end) {
 			System.out.println("Enter command");
-			String command = input.next();
+			String command = input.nextLine();
 			if (command.equals("exit"))
 			{
 				end = true;
 				System.out.println("You have ended the game");
 				System.exit(0);
 			}
-			Parse.parse(playerOne, command);
-		}
 		
+			String result = Parser.parse(playerOne,command);
+			System.out.println(result);
+		}
 		
 	}
 
