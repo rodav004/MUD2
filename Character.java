09f7108edf8a1c66dab6e3b3120a83dfeecdb14e
calam002 <32172869+calam002@users.noreply.github.com>
2017-10-03 12:32:52 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,4 +1,3 @@
-
 package mud;
 import java.util.ArrayList;
 
@@ -12,7 +11,10 @@ public class Character extends GameObject {
 		this.location = location;
 		this.inventory = inventory;
 	}
-	
+	/**
+	 * 
+	 * @return an ArrayList of the names of the items in inventory
+	 */
 	public ArrayList<String> getItems() {
 		ArrayList<Item> items = this.inventory;
 		ArrayList<String> itemNames = new ArrayList<>();
@@ -21,35 +23,41 @@ public class Character extends GameObject {
 		}
 		return itemNames;
 	}
-	//Add an item to inventory
+	/**
+	 * addItem adds an item to the inventory
+	 * @param itemName takes in the item to be added to inventory
+	 * @return result a String declaring whether the item was added or not
+	 */
+	
 	public String addItem(String itemName)
 	{
-		ArrayList<Item> roomItems = location.items;
-		String result = "";
+		ArrayList<Item> roomItems = new ArrayList<>(location.items);
+		String result = null;
 		for (Item items: roomItems)
 		{
-			if (items.getName().equals(itemName))
+			if (items.name.equals(itemName))
 			{
 				Item foundItem = items;
 				inventory.add(foundItem);
 				roomItems.remove(foundItem);
 				result = "Hooray! The item " + foundItem.getName() + " is now in your inventory. The description is " + foundItem.getDescription() + ".";
 			}
-			else
-			{
-				result = "Sorry! The item is not in this room.";
-			}
+			else result = "Sorry! The item is not in this room.";
 		}
 		return result;
 	}
-	
-        public String removeItem(String itemName)
+	/**
+	 * removeItem removes an item from inventory
+	 * @param itemName method takes in the name of the item to be found
+	 * @return String declaring if the item has been removed or if the item is not in inventory
+	 */
+	public String removeItem(String itemName)
 	{
-		String result = "";
-		ArrayList<Item> inventoryItems = this.inventory;
-		ArrayList<Item> roomItems = location.items;
-		for (Item items : inventoryItems)
-			if (items.getName().equals(itemName))
+		String result = null;
+		ArrayList<Item> inventoryItems = new ArrayList<>(this.inventory);
+		ArrayList<Item> roomItems = new ArrayList<>(location.items);
+		for (Item items : inventoryItems) {
+			if (items.name.equals(itemName))
 			{
 				Item foundItem = items;
 				roomItems.add(foundItem);
@@ -57,9 +65,15 @@ public class Character extends GameObject {
 				result = "The item " + itemName + " has been removed from your inventory and is in " + location + ".";
 			}
 			else result = "The item " + itemName + " is not in your inventory.";
+			}
 		return result;
 	}
 	
+	/**
+	 * move changes the room a character is in
+	 * @param direction checks for door and moves in said direction
+	 * @return String stating whether or not the character has moved rooms
+	 */
 	public String move(String direction) {
 		String newRoom = null;
 		
