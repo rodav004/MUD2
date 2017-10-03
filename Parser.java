d77e3bff42aa484c60ae23fda01f4a6642af8ec3
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-10-03 12:28:41 -0400

    removed mistakes so it actually works now >.<

--- Parser.java
+++ Parser.java
@@ -1,31 +1,28 @@
 package mud;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Parser {
 	
 	public static String parse(Character playerOne, String input) {
-		List<String> splitStrings = Arrays.asList(input.split(" "));
+		String[] splitStrings = input.split(" ");
+		String r = "test";
 		
-		if (splitStrings.get(0).equals("exit")) {
+		if (splitStrings[0].equals("exit")) {
 			System.exit(0);
-			String r = "Exiting the game...";
+			r = "Exiting the game... Goodbye!";
 		}
-		else if (splitStrings.get(0).equals("move")) {
-			playerOne.move(splitStrings.get(1));
-			String r = playerOne.move(splitStrings.get(1));
+		else if (splitStrings[0].equals("move")) {		
+			r = playerOne.move(splitStrings[1]);
 		}
 		
-		else if (splitStrings.get(0).equals("get")) {
-			playerOne.addItem(splitStrings.get(1));
-			String r = playerOne.addItem(splitStrings.get(1));
+		else if (splitStrings[0].equals("get")) {
+			r = playerOne.addItem(splitStrings[1]);
 		}
-		else if (splitStrings.get(0).equals("drop")) {
-			playerOne.removeItem(splitStrings.get(1));
-			String r = playerOne.removeItem(splitStrings.get(1));
+		else if (splitStrings[0].equals("drop")) {
+			r = playerOne.removeItem(splitStrings[1]);
 		}
 		else {
-			String r = "Oops! I don't know what you're trying to say!";
+			r = "Oops! I don't know what you're trying to say!";
 		}
 		return r;
 	}
