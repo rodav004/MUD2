94833c26775994fb8baa77cd8f0d33f9b5584973
calam002 <32172869+calam002@users.noreply.github.com>
2017-10-23 12:20:26 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,16 +1,44 @@
 package mud;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JList;
 public class UserInterface {
 	
 	private static Scanner input;
+	private static Scanner input;
+	private JTextField inputBox;
+	private JList<String> displayInventory;
+	private JList<String> displayRoomItems;
+	private JLabel displayActions;
+	
+	public UserInterface() {
+		Container cp = getContentPane();
+		cp.setLayout(new BorderLayout());
+		
+		inputBox.setEditable(true);
+		inputBox.setText("Enter command");
+		displayActions.setText("What is your name?");
+		String name = inputBox.getText();
+		
+		cp.add(displayActions, BorderLayout.NORTH);
+		cp.add(inputBox, BorderLayout.SOUTH);
+		cp.add(displayInventory, BorderLayout.WEST);
+		cp.add(displayRoomItems, BorderLayout.EAST);
+	}
 
 	public static void main(String[] args) {
 		
 		Game.start();
 
-		input = new Scanner(System.in);
+		/*input = new Scanner(System.in);
 		System.out.println("What is your name?");
 		String name = input.nextLine();
 		ArrayList<Item> inventory = new ArrayList<Item>();
@@ -31,8 +59,12 @@ public class UserInterface {
 			}
 			String result = Parser.parse(playerOne, command);
 			System.out.println(result);
-		}
-		
+		}*/
+		UserInterface UI = new UserInterface();
+		UI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		UI.setTitle(Game.room1.getName());
+		UI.setSize(500,250);
+		UI.setVisible(true);
 		
 	}
 
