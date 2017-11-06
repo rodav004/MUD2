c41610075a830e51a401b240cfb2bc0285b59a12
calam002 <32172869+calam002@users.noreply.github.com>
2017-11-06 12:05:47 -0500

    Update UserInterface.java
    Items are now displayed in buttons. Yay! I also added getSingularItem to room so make sure to update that as well.

--- UserInterface.java
+++ UserInterface.java
@@ -8,6 +8,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JPanel;
+import javax.swing.JList;
+import javax.swing.JLabel;
 
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
@@ -15,42 +17,53 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+
 public class UserInterface {
 	
+		
 	private JTextField inputBox;
 	private JTextField outBox;
 	private JPanel inputPanel;
 	private JButton inputButton;
 	public static int inc = 0;
 	private Character playerOne;
+	
+	private JPanel roomItemPanel;
+	private JButton roomItemOne = new JButton();
+	private JButton roomItemTwo = new JButton();
+	private JTextField inventoryItems = new JTextField();
+	private JLabel roomName = new JLabel();
 
 	public JFrame getFrame() {
 		return this.frm;
 	}
 	private JFrame frm;
 
-	public UserInterface() {
+	public UserInterface() { 
+		
 		this.frm = new JFrame("MUD");
 		frm.setSize(500,300);
 
-		/*ArrayList<String> roomItemName = playerOne.location.getItems();
-		ArrayList<String> inventoryItemName = playerOne.getItems();
-		String[] roomItNa = (String[]) roomItemName.toArray();
-		String[] inventoryItNa = (String[])inventoryItemName.toArray();
-		roomItems = new JList<String>(roomItNa);
-		inventoryItems = new JList<String>(inventoryItNa); */
-		
 		this.inputBox = new JTextField(10);
 
 		inputBox.setEditable(true);
 		inputBox.setText("Enter name");
-		inputBox.addMouseListener(new MouseListener() {
+				inputBox.addMouseListener(new MouseListener() {
+			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (!(inputBox.getText().equals("Enter name") || inputBox.getText().equals("Enter command"))) {	}
+				if (!(inputBox.getText().equals("Enter name") || inputBox.getText().equals("Enter command"))) {
+				}
 				else {
 					inputBox.setText("");
 				}
 			}
+			@Override
+			public void mousePressed(MouseEvent e) {	
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+			@Override
 			public void mouseEntered(MouseEvent e) {
 				if (!(inputBox.getText().equals("Enter name") || inputBox.getText().equals("Enter command"))) {
 				}
@@ -58,10 +71,12 @@ public class UserInterface {
 					inputBox.setText("");
 				}
 			}
+			@Override
 			public void mouseExited(MouseEvent e) {
 				if (!(inputBox.getText().equals("Enter name")
 					|| inputBox.getText().equals("Enter command")
-					|| inputBox.getText().equals(""))) { }
+					|| inputBox.getText().equals(""))) {	
+				}
 				else if (inc == 0) {
 						inputBox.setText("Enter name");
 				}
@@ -69,48 +84,66 @@ public class UserInterface {
 					inputBox.setText("Enter command");
 				}
 			}
-			public void mousePressed(MouseEvent e) { }
-			public void mouseReleased(MouseEvent e) { }
 		});
-		
 		this.inputButton = new JButton("Submit");
 		this.outBox = new JTextField();
 		outBox.setEditable(false);
+		roomItemOne.hide();
+		roomItemTwo.hide();
 		
-		inputButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				if (inc == 0) {
+			inputButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if (inc == 0) {
 					String name = inputBox.getText();
 					ArrayList<Item> inventory = new ArrayList<Item>();
 					playerOne = new Character(name, "You have no description yet", Game.room1, inventory);
 					outBox.setText("Hello " + name + "! You are in the " + Game.room1.getName());
+					roomName.setText(Game.room1.getName());
+					roomItemOne.setText(playerOne.location.getSingularItem(0));
+					roomItemTwo.setText(playerOne.location.getSingularItem(1));
+					roomItemOne.show();
+					roomItemTwo.show();
 					inputBox.setText("Enter command");
 					inc++;
+					}
+			else {
+				String action = inputBox.getText();
+				String result = Parse.parse(playerOne, action);
+				if (playerOne.location.equals(Game.room1)) {
+					roomName.setText(Game.room1.getName());
+					roomItemOne.setText(playerOne.location.getSingularItem(0));
+					roomItemTwo.setText(playerOne.location.getSingularItem(1));
 				}
 				else {
-				String action = inputBox.getText();
-				String result = Parser.parse(playerOne, action);
+					roomName.setText(Game.room2.getName());
+					roomItemOne.setText(playerOne.location.getSingularItem(0));
+					roomItemTwo.setText(playerOne.location.getSingularItem(1));
+				}
 				outBox.setText(result);
 				inputBox.setText("");
 				}
 			}
-		});
+		}); 		
 
 		this.inputPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
 		inputPanel.add(inputBox);
 		inputPanel.add(inputButton);
-
+		
+		roomItemPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		roomItemPanel.add(roomName);
+		roomItemPanel.add(roomItemOne);
+		roomItemPanel.add(roomItemTwo);
+		
 		Container cp = frm.getContentPane();
 		cp.add(outBox, BorderLayout.CENTER);
-		//cp.add(displayActions, BorderLayout.NORTH);
 		cp.add(inputPanel, BorderLayout.SOUTH);
-		//cp.add(displayInventory, BorderLayout.WEST);
-		//cp.add(displayRoomItems, BorderLayout.EAST);
+		cp.add(roomItemPanel, BorderLayout.WEST);
 	}
 
 	public static void main(String[] args) {
 		
 		Game.start();
+		inc = 0;
 
 		/*input = new Scanner(System.in);
 		System.out.println("What is your name?");
