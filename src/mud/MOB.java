3556825e3ed0c669c24c721ceab6b87018ccb372
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-11-28 12:11:49 -0500

    extends character
    added a mob item and a line of speech

--- src/mud/MOB.java
+++ src/mud/MOB.java
@@ -1,9 +1,29 @@
 package mud;
 
+import java.util.ArrayList;
 import java.util.Random;
 
-public class MOB {
-	private rand = new Random();
+public class MOB extends Character {
+	
+	private Item mobItem;
+	private String speech;
+	
+	public MOB(String name, String description, Room location, Item mobItem, String speech) {
+		super(name, description, location);
+		this.mobItem = mobItem;
+		this.speech = speech;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Item getMobItem(){
+		return mobItem;
+	}
+	
+	public String getSpeech() {
+		return speech;
+	}
+	
+	private Random rand = new Random();
 	public void step() {
 	}
 }
