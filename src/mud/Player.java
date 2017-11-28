ca406085b6f98a7fb55286d97ca875303a0c4a4f
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-11-28 12:12:59 -0500

    updated to extend character

--- src/mud/Player.java
+++ src/mud/Player.java
@@ -1,5 +1,78 @@
 package mud;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player extends Character {
+	
+	private ArrayList<Item> inventory;
+
+	public Player(String name, String description, Room location, ArrayList<Item> inventory) {
+		super(name, description, location);
+		this.inventory = inventory;
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * 
+	 * @return an ArrayList of the names of the items in inventory
+	 */
+	public ArrayList<String> getItems() {
+		ArrayList<Item> items = this.inventory;
+		ArrayList<String> itemNames = new ArrayList<>();
+		for (Item item : items) {
+			itemNames.add(item.name);
+		}
+		return itemNames;
+	}
+	/**
+	 * addItem adds an item to the inventory
+	 * @param itemName takes in the item to be added to inventory
+	 * @return result a String declaring whether the item was added or not
+	 */
+	
+	public String addItem(String itemName)
+	{
+		ArrayList<Item> roomItems = new ArrayList<>(location.items);
+		String result = null;
+		for (Item items: roomItems)
+		{
+			if (items.name.equals(itemName))
+			{
+				Item foundItem = items;
+				inventory.add(foundItem);
+				location.items.remove(foundItem);
+				result = "Hooray! The item " + foundItem.getName() + " is now in your inventory. The description is: " + foundItem.getDescription();
+				break;
+			}
+			else result = "Sorry! The item is not in this room.";
+		}
+		return result;
+	}
+	/**
+	 * removeItem removes an item from inventory
+	 * @param itemName method takes in the name of the item to be found
+	 * @return String declaring if the item has been removed or if the item is not in inventory
+	 */
+	public String removeItem(String itemName)
+	{
+		String result = null;
+		ArrayList<Item> inventoryItems = new ArrayList<>(this.inventory);
+		for (Item items : inventoryItems) {
+			if (items.name.equals(itemName))
+			{
+				Item foundItem = items;
+				location.items.add(foundItem);
+				inventory.remove(foundItem);
+				result = "The item " + itemName + " has been removed from your inventory and is in the " + location.name + ".";
+				break;
+			}
+			else result = "The item " + itemName + " is not in your inventory.";
+			}
+		return result;
+	}
+	
+	public String look() {
+		return location.getDescription();
+	}
+	
 }
