98337c4122dd1fce5a96d64d4ae8e70573a8dbb1
calam002 <32172869+calam002@users.noreply.github.com>
2017-11-30 12:50:38 -0500

    Update MobScheduler.java

--- src/mud/MobScheduler.java
+++ src/mud/MobScheduler.java
@@ -1,2 +1,45 @@
-public class MobScheduler {
+package mud;
+import java.util.ArrayList;
+import java.util.Random;
+public class MobScheduler extends MOB implements Runnable{
+
+	public MobScheduler(String name, String description, String phrase, Room location, ArrayList<Item> item) {
+		super(name, description, phrase, location, item);
+	}
+
+	public Runnable moveRoom() {
+		return () -> {
+			Random rand = new Random();
+			int wait = rand.nextInt(30) + 1;
+			int door = rand.nextInt(3);
+			if (location.hasDoor(door)) {
+				String direction;
+				if (door == 0) {
+					direction = "north";
+				}
+				if (door == 1) {
+					direction = "east";
+				}
+				if (door == 2) {
+					direction = "south";
+				}
+				else {
+					direction = "west";
+				}
+				try {
+					Thread.sleep(wait*1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				MOB.move(direction);
+			}
+		};
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub	
+	}
+
 }
+
