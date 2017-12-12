ddd783e59d9e98bda38ab4c0e80103b6500fd399
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-12-12 14:16:23 -0500

    Update Character.java

--- src/mud/Character.java
+++ src/mud/Character.java
@@ -1,17 +1,72 @@
 package mud;
-
 import java.util.ArrayList;
 
 public class Character extends GameObject {
-	Room location;
+	public Room location;
+	ArrayList<Item> inventory;
 	
-	public Character(String name, String description, Room location) {
+	public Character(String name, String description, Room location, ArrayList<Item> item) {
 		this.description = description;
 		this.name = name;
 		this.location = location;
-		this.location.characters.add(this);
+		this.inventory = item;
 	}
+	
+	/* public String getSingularItem(int index) {
+		return inventory.get(index).getName();
+	} */
+	
+	/**
+	 * addItem adds an item to the inventory
+	 * @param itemName takes in the item to be added to inventory
+	 * @return result a String declaring whether the item was added or not
+	 */
+	
+	public String addItem(String itemName)
+	{
+		ArrayList<Item> roomItems = new ArrayList<>(location.items);
+		String result = null;
+		if (roomItems.isEmpty()) {
+			result = "There are no items in this room."; }
+		for (Item items: roomItems)
+		{
+			String roomItemName = items.name;
+			if (roomItemName.equals(itemName))
+			{
+				Item foundItem = items;
+				inventory.add(foundItem);
+				location.items.remove(foundItem);
+				result = "Hooray! The item " + foundItem.getName() + " is now in your inventory. The description is: " + foundItem.getDescription() + ".";
+				break;
+			}
+			else result = "Sorry! The item is not in this room.";
+		}
+		return result;
+	}
+	/**
+	 * removeItem removes an item from inventory
+	 * @param itemName method takes in the name of the item to be found
+	 * @return String declaring if the item has been removed or if the item is not in inventory
+	 */
+	public String removeItem(String itemName)
+	{
+		String result = null;
+		ArrayList<Item> inventoryItems = new ArrayList<>(this.inventory);
+		if (inventory.isEmpty()) {
+			result = "Sorry, your inventory is empty.";  }
 
+		for (Item items : inventoryItems) {
+			if (items.name.equals(itemName))
+			{
+				Item foundItem = items;
+				location.items.add(foundItem);
+				inventory.remove(foundItem);
+				result = "The item " + itemName + " has been removed from your inventory and is in " + location.name + ".";
+			}
+			else result = "The item " + itemName + " is not in your inventory.";
+			}
+		return result;
+	}
 	
 	/**
 	 * move changes the room a character is in
@@ -20,55 +75,54 @@ public class Character extends GameObject {
 	 */
 	public String move(String direction) {
 		String newRoom = null;
-
-		direction = direction.toLowerCase();
-		Door exit = null;
-		boolean validDirection = true;
-
-		switch (direction) {
-			case "up":
-			case "north":
-				exit = location.doors[0];
-				break;
-			case "down":
-			case "south":
-				exit = this.location.doors[2];
-				break;
-			case "right":
-			case "east":
-				exit = location.doors[1];
-				break;
-			case "left":
-			case "west":
-				exit = location.doors[3];
-				break;
-			default:
-				validDirection = false;
-				break;
-		}
-
-		if (validDirection && exit != null) {
-			newRoom = "You enter the " + exit.room.name + ". "+ exit.room.description;
-			synchronized (this) {
-				this.location.characters.remove(this);
-				this.location = exit.room;
-				this.location.characters.add(this);
+		
+		if (direction.equals("north") || direction.equals("up")) {
+			Door exit = location.doors[0];
+			if (exit != null) {
+				newRoom = "You enter the " + exit.room.name + ". "+ exit.room.description;
+				location = exit.room;
+			}
+			else {
+				newRoom = "There is not a door in that direction!";
 			}
 		}
-		else if (validDirection && exit == null) {
-			newRoom = "There is no door in that direction!";
+		else if (direction.equals("south") || direction.equals("down")) {
+			Door exit = location.doors[2];
+			if (exit != null) {
+				newRoom = "You enter the " + exit.room.name + ". "+ exit.room.description;
+				location = exit.room;
+			}
+			else {
+				newRoom = "There is not a door in that direction!";
+			}
 		}
-		else if (!validDirection) {
+		else if (direction.equals("east") || direction.equals("right")) {
+			Door exit = location.doors[1];
+			if (exit != null) {
+				newRoom = "You enter the " + exit.room.name + ". "+ exit.room.description;
+				location = exit.room;
+			}
+			else {
+				newRoom = "There is not a door in that direction!";
+			}
+		}
+		else if (direction.equals("west") || direction.equals("left")) {
+			Door exit = location.doors[3];
+			if (exit != null) {
+				newRoom = "You enter the " + exit.room.name + ". "+ exit.room.description;
+				location = exit.room;
+			}
+			else {
+				newRoom = "There is not a door in that direction!";
+			}
+		}
+		else{
 			newRoom = "Sorry, that is not a valid direction!";
 		}
-
+		
 		return newRoom;
-
+		
 	}
 	
-	public String look() {
-		return location.getDescription();
-	}
 	
-
 }
