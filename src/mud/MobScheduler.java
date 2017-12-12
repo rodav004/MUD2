c96825d29432bbb63f4be82cfbe5ab40e450e7b7
rodav004 <32172844+rodav004@users.noreply.github.com>
2017-12-12 14:18:10 -0500

    Update MobScheduler.java

--- src/mud/MobScheduler.java
+++ src/mud/MobScheduler.java
@@ -1,12 +1,10 @@
 package mud;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-import java.util.concurrent.*;
-import java.util.stream.IntStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class MobScheduler {
-
+public class MobScheduler{
+	
 	private MobScheduler() {
 	}
 
@@ -14,48 +12,38 @@ public class MobScheduler {
 
 	public static void scheduleMOB(MOB theMob) {
 		threadPool.submit(MobScheduler.runnableForMob(theMob));
-	}
-
+        }
+		
 	public static Runnable runnableForMob(MOB theMob) {
 		return () -> {
+			Random rand = new Random();
 			while (true) {
-				Random rand = new Random();
-				int wait = rand.nextInt(30) + 1;
-
-				List<Integer> validDirections = new ArrayList<>();
-				Door[] theDoors = theMob.location.doors;
-				for (int i = 0; i < theDoors.length; i++) {
-					if (theDoors[i] != null) validDirections.add(i);
-				}
-				if (validDirections.size() == 0) continue;
-				int door = validDirections.get(rand.nextInt(validDirections.size()));
+			int wait = rand.nextInt(15) + 1;
+			int door = rand.nextInt(4);
+			//System.out.println(theMob.getName() + ": door is " + door);
+			//System.out.println(theMob.getName() + ": wait is " + wait);
+			if (theMob.location.hasDoor(door)) {
 				String direction;
 				switch (door) {
-					case 0:
-						direction = "north";
-						break;
-					case 1:
-						direction = "east";
-						break;
-					case 2:
-						direction = "south";
-						break;
-					case 3:
-						direction = "west";
-						break;
-					default:
-						direction = null;
-						break;
+					case 0: direction = "north"; break;
+					case 1: direction = "east";  break;
+					case 2: direction = "south"; break;
+					case 3: direction = "west";  break;
+					default: direction = null;   break;
 				}
-				theMob.move(direction);
-				UserInterface.mobDidMove(theMob);
-
-				try {
-					Thread.sleep(wait * 1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+				if (direction != null) {
+					theMob.move(direction);
+					System.out.println(theMob.move(direction));
+					theMob.update();
 				}
 			}
+			try {
+				Thread.sleep(wait * 1000);	
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		};
 	}
+	
 }
